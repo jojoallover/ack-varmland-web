8fa54429b4f1eab2303826fd9ed10cffcdd46385
Jonas Jonsson <jojo.all.over@gmail.com>
2025-11-22 22:57:36 +0100

    Manual build to docs folder

---
+++ docs/js/light-mode-toggle.js
@@ -0,0 +1,4 @@
+document.getElementById('light-mode-toggle').addEventListener('click', function() {
+    alert('Warning: Light mode attracts bugs. Please remain in dark mode.');
+    // Alternativt: Skriv ut varningen i den virtuella terminalen
+});
